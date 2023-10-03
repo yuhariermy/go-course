33688a8d62765d9cb0b1a0555ea57ef54ca3beb5
ermyyuhari <yuhariermy@gmail.com>
2023-10-03 22:04:21 +0700

    Write into File, and Read From the file

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 type deck []string
 
@@ -28,4 +33,25 @@ func (d deck) print() {
 // For Slicing
 func slicingFunc(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+// For change slicing to string
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+// save to file after run it, (d deck) was receiver. it could be use cards.saveToFile
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666) // 0666 is default err, for can write and read
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	ss := strings.Split(string(bs), ",")
+	return deck(ss)
 }
--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,20 @@
 package main
 
 func main() {
-	cards := newDeck()
+	//cards := newDeck()
+	/* =======IT USED FOR SLICING======= */
+	//
+	//hand, remainingCards := slicingFunc(cards, 5)
+	//hand.print()
+	//remainingCards.print()
 
-	hand, remainingCards := slicingFunc(cards, 5)
-	hand.print()
-	remainingCards.print()
+	/* =======IT USED FOR CHANGE TO STRING======= */
+	//fmt.Println(cards.toString())
+
+	/* =======IT USED FOR Write To File Function======= */
+	//cards.saveToFile("saveToFile_cards")
+
+	/* =======IT USED FOR Read the from the File======= */
+	readCardsFromTheFile := newDeckFromFile("saveToFile_cards")
+	readCardsFromTheFile.print()
 }
